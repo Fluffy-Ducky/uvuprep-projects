3abc075e8faceef16d3bda13e7f493b1e896ba6e
Fluffy-Ducky <106999959+Fluffy-Ducky@users.noreply.github.com>
2022-07-14 10:45:19 -0600

    Update students2022.js

--- src/json/students2022.js
+++ src/json/students2022.js
@@ -4,6 +4,7 @@ const students = {
     "https://replit.com/@buckldav/DinoGameP5Starter",
     "https://replit.com/@buckldav/PizzaStarter",
   ],
+  "Annissa N" : ["https://Final-Project-Elemental-Ducks.fluffy-ducky.repl.co"],
 };
 
 const studentsSorted = Object.keys(students)
